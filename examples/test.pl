22769eb0d64e06f361fc7b37570335d13c9246ad
Fayland Lam <fayland@gmail.com>
2014-06-28 11:28:22 +0800

    add payment

---
+++ examples/test.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use lib "$Bin/../lib";
+use Business::PayPoint::MCPE;
+use Data::Dumper;
+
+my $bpm = Business::PayPoint::MCPE->new(
+    TestMode => 1,
+    InstID => '12345',
+);
+
+my $data = $bpm->payment(
+    CartID => 654321,
+    Desc   => 'description of goods',
+    Amount => '10.00',
+    Currency => 'GBP',
+    CardHolder => 'Joe Bloggs',
+    Postcode   => 'BA12BU',
+    Email      => 'test@paypoint.net',
+    CardNumber => '1234123412341234',
+    CV2        => '707',
+    ExpiryDate => '0616',
+    CardType   => 'VISA',
+    Country    => 'GB',
+);
+print Dumper(\$data);
+
+1;
--- lib/Business/PayPoint/MCPE.pm
+++ lib/Business/PayPoint/MCPE.pm
@@ -4,6 +4,68 @@ use strict;
 use 5.008_005;
 our $VERSION = '0.01';
 
+use LWP::UserAgent;
+use Carp 'croak';
+
+sub new {
+    my $class = shift;
+    my %args  = @_ % 2 ? %{$_[0]} : @_;
+
+    $args{InstID} or croak "InstID is required.";
+
+    $args{TestMode} ||= 0;
+    $args{ua} ||= LWP::UserAgent->new();
+
+    $args{POST_URL} ||= 'https://secure.metacharge.com/mcpe/corporate';
+    $args{APIVersion} ||= '1.3';
+
+    bless \%args, $class;
+}
+
+sub payment {
+    my $self = shift;
+    my %args = @_ % 2 ? %{$_[0]} : @_;
+
+    $self->request(
+        %args,
+        TransType  => 'PAYMENT',
+    );
+}
+
+sub request {
+    my $self = shift;
+    my %params = @_ % 2 ? %{$_[0]} : @_;
+
+    my @intFields = ('TestMode', 'InstID', 'TransID', 'AccountID', 'AuthMode', 'CountryIP', 'AVS', 'Status', 'Time', 'CV2', 'Reference', 'Recurs', 'CancelAfter', 'ScheduleID');
+    my @fltFields = ('APIVersion', 'Amount', 'OriginalAmount', 'SchAmount', 'FraudScore');
+    my @datFields = ('Fulfillment');
+
+    $params{TestMode}   ||= $self->{TestMode};
+    $params{InstID}     ||= $self->{InstID};
+    $params{APIVersion} ||= $self->{APIVersion};
+
+    my %r;
+    foreach my $key (keys %params) {
+        if ($key =~ /^(int|flt|str|dat)/) {
+            $r{$key} = $params{$key};
+        } elsif (grep { $_ eq $key } @intFields) {
+            $r{'int' . $key} = $params{$key};
+        } elsif (grep { $_ eq $key } @fltFields) {
+            $r{'flt' . $key} = $params{$key};
+        } elsif (grep { $_ eq $key } @fltFields) {
+            $r{'dat' . $key} = $params{$key};
+        } else {
+            $r{'str' . $key} = $params{$key};
+        }
+    }
+
+    my $resp = $self->{ua}->post($self->{POST_URL}, \%r);
+    use Data::Dumper; print STDERR Dumper(\$resp);
+    unless ($resp->is_success) {
+        return { error => $resp->status_line };
+    }
+}
+
 1;
 __END__
 
